ab9f3741a9048688f02e30be39e2209008dde167
Nicholas Sandomeno <nicholassandomeno@Nicholass-MBP.fios-router.home>
2023-01-12 20:04:20 -0500

    step 3 handle multiple methods

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,37 @@
-use tokio::io::AsyncWriteExt;
+use std::fmt::format;
+use std::str;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
 async fn handle_connection(mut stream: TcpStream) {
-    let contents = "{\"balance\": 0.00}";
+    // Read first 16 characters from incoming stream.
+    let mut buffer = [0; 16];
+    stream.read(&mut buffer).await.unwrap();
+    // First 4 characters are used to detect the HTTP Request Method
+    let method_type = match str::from_utf8(&buffer[0..4]) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    let contents = match method_type {
+        "GET" => {
+            // TODO return real balance.
+            format!("{{\"balance\": {}}}", 0.0)
+        }
+        "POST" => {
+            // Take characters after 'POST /' until whitespace is detected.
+            let input: String = buffer[6..16].iter()
+                                    .take_while(|x| **x != 32u8)
+                                    .map(|x| *x as char)
+                                    .collect();
+            let balance_update = input.parse::<f32>().unwrap();
+            // TODO add balance update handling.
+            format!("{{\"balance\": {}}}", balance_update)
+        }
+        _ => {
+            panic!("Invalid HTTP Method!")
+        }
+    };
 
     let response = format!(
         "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: {}\r\n\r\n{}",
